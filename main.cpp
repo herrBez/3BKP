a2acc0ffc8c9779d2dc58e28abb237ddfe84a0e0
herrBez <mirko.bez@studenti.unipd.it>
2017-04-23 16:57:21 +0200

    Started a major refactoring to reduce the size of the single files and make the code easier to understand

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@
 #include <unistd.h>
 #include "Instance3BKP.h"
 #include <assert.h>     /* assert */
-
+#include "utils.h"
 #include <algorithm>
 
 
@@ -54,105 +54,6 @@ static struct option long_options[] = {
 	{"ignore-knapsack-cost", no_argument, 0, 'i'}, /* Disable optimization of knapsacks */
 	{0, 0, 0, 0},
 };
-
-
-/**
- * Struct that contains all the vectors used to keep track of the variables
- */
-struct mapVar {
-	private : 
-	mapVar() : T(0, vector< int >(0)), B(0, vector<vector<vector<int>>>(0, vector<vector<int>>(0, vector< int >(0)))),
-			   Rho(0, vector< int >(0)), Chi(0, vector< vector<int> >(0, vector<int>(0))) { }
-	public : 
-	vector< vector < int > > T;
-	vector< vector < vector < vector < int > > > > B;
-	vector< vector < int > > Rho;
-	vector< vector < vector <int > > > Chi;
-	vector< int > Z;
-	
-	mapVar(int K, int N, int R){
-		T.resize(K);
-		for(auto &i : T)
-			i.resize(N);
-		
-		B.resize(K);
-		for(auto &i : B) {
-			i.resize(N);
-			for(auto &k : i) {
-				k.resize(N);
-				for(auto &j : k) {
-					j.resize(3);
-				}
-			}
-		}
-		
-		Rho.resize(N);
-		for(auto &i : Rho){
-			i.resize(R);
-		}
-		
-		Chi.resize(K);
-		for(auto &k : Chi){
-			k.resize(N);
-			for(auto &i : k){
-				i.resize(3);
-			}
-		}
-		Z.resize(K);
-	}
-};
-/**
- * Struct that contains all the vectors used to keep track of the variables
- */
-struct VarVal {
-	private : 
-	VarVal() : t(0, vector< int >(0)), b(0, vector<vector<vector<int>>>(0, vector<vector<int>>(0, vector< int >(0)))),
-			   rho(0, vector< int >(0)), chi(0, vector< vector<double> >(0, vector<double>(0))), rotation(0, vector<int>(0)) { }
-	public : 
-	vector< vector< int > > t;
-	vector< vector < vector < vector < int > > > > b;
-	vector< vector < int > > rho;
-	vector< vector < vector < double > > > chi;
-	vector< int > z;
-	vector< vector< int > > rotation;
-	
-	VarVal(int K, int N, int R){
-		t.resize(K);
-		for(auto &i : t)
-			i.resize(N);
-		
-		b.resize(K);
-		for(auto &i : b) {
-			i.resize(N);
-			for(auto &k : i) {
-				k.resize(N);
-				for(auto &j : k) {
-					j.resize(3);
-				}
-			}
-		}
-		
-		rho.resize(N);
-		for(auto &i : rho){
-			i.resize(R);
-		}
-		
-		chi.resize(K);
-		for(auto &k : chi){
-			k.resize(N);
-			for(auto &i : k){
-				i.resize(3);
-			}
-		}
-		z.resize(K);
-		
-		rotation.resize(N);
-		for(auto &r : rotation){
-			r.resize(3);
-		}
-	}
-};
-
 
 
 
