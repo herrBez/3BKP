9b48846b58a665cb362dd715f8f7eac95c022312
herrBez <mirko.bez@studenti.unipd.it>
2017-05-25 23:29:26 +0200

    Improved analyser.R to create a .tex file containing a table summarizing the results

--- Benchmark/analyser.R
+++ Benchmark/analyser.R
@@ -2,8 +2,71 @@
 args <- commandArgs(TRUE)
 printf <- function(...) invisible(print(sprintf(...)))
 
+analyse <- function(mydata) {
+	
+	return(mean(mydata$CPU.Time));
+}
 
-mydata = read.csv(args[1], header=TRUE, sep=",")
-printf("%f", mean(mydata$CPU.Time), mean(mydata$User.Time))
-printf("%f", max(mydata$CPU.Time), max(mydata$User.Time))
-printf("%f", min(mydata$CPU.Time), min(mydata$User.Time))
+#mydata = read.csv(args[1], header=TRUE, sep=",")
+filenames <- list.files("OneKnapsack", pattern="\\.multi\\.csv$", full.names=TRUE)
+
+con <- file("filename.tex", "w")
+cat(
+	sprintf("%s \n %s \n %s \n %s \n %s\n %s\n %s \n %s \n",
+		"\\documentclass{article}",
+		"\\begin{document}",
+		"\\begin{table}",
+		"\\begin{center}",
+		"\\begin{tabular}{| c | c | c | c | c | c | c |}",
+		"\\hline",
+		"instance & completed & avg cpu used & average & max & min & CV \\% \\\\",
+		"\\hline")
+	,
+	file=con)
+		
+	
+
+ldf <- lapply(filenames, function(i){
+	mydata<-read.csv(i, header=TRUE, skip=",")
+	if(length(mydata$CPU.Time) > 0){
+		cat(
+			sprintf("\\verb|%s| & %d/7&%.2f&%.4f&%.4f&%.4f&%.4f \\\\ \n", 
+					basename(i),
+					length(mydata$User.Time),
+					mean(mydata$CPU.Time)/mean(mydata$User.Time),
+					mean(mydata$User.Time),
+					max(mydata$User.Time),
+					min(mydata$User.Time),
+					(sd(mydata$User.Time)/mean(mydata$User.Time))*100), 
+			file=con
+		)
+	} else {
+		cat(
+			sprintf("\\verb|%s| & %d & %s & %s & %s & %s & %s \\\\ \n", 
+					basename(i),
+					0,
+					"NA",
+					"NA",
+					"NA",
+					"NA",
+					"NA"
+					), 
+			file=con
+		)
+	}
+})
+
+cat(sprintf("\\hline\n\\end{tabular}\n\\end{center}\n\\end{table}\n\\end{document}"), file = con);
+
+close(con);
+
+
+##result = lapply(ldf, analyse);
+
+#EXIT if no data available
+#if(length(mydata$CPU.time) < 1){
+#	stop("No data available");
+#}
+#printf("%f", mean(mydata$CPU.Time), mean(mydata$User.Time))
+#printf("%f", max(mydata$CPU.Time), max(mydata$User.Time))
+#printf("%f", min(mydata$CPU.Time), min(mydata$User.Time))
