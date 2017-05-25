fe02a06c045fc4ad16ae965989a263cdf4049bc8
herrBez <mirko.bez@studenti.unipd.it>
2017-05-26 00:28:19 +0200

    Improved benchmark so that it uses variables --> future extension command line parameters

--- Benchmark/benchmark.sh
+++ Benchmark/benchmark.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
 iteration_per_instance=7
-mkdir OneKnapsack
-rm OneKnapsack/*.csv
-for filename in ../MultiInstance/OneKnapsack/*.dat; do
+output_directory=TwoKnapsack
+program=../Multi3BKP/main
+program_type=multi
+
+
+mkdir $output_directory
+rm $output_directory/*.csv
+for filename in ../MultiInstance/$output_directory/*.dat; do
 	echo $filename
 	last_part_of_path=`basename -s .dat $filename`
 	echo $last_part_of_path
-	output=OneKnapsack/$last_part_of_path.small.csv
+	output=$output_directory/$last_part_of_path.multi.csv
 	echo "create $output"
 	echo "User.Time, CPU.Time" > $output
 	for i in {1..7}; do
-		out=`timeout 15m ../SmallHull/main $filename -b -x -y -z -w 6`
+		out=`timeout 20m $program $filename -b -x -y -z -w 6`
 		if [ $? -eq 124 ]; then
 			echo "Timeout Reached. $output not successful";
 		else
