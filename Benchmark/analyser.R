0308a635673e93f2ff1eec8034c993fc34234a61
herrBez <mirko.bez@studenti.unipd.it>
2017-06-17 23:22:17 +0200

    Improved the benchmark script and the benchmark analyzer

--- Benchmark/analyser.R
+++ Benchmark/analyser.R
@@ -10,16 +10,17 @@ analyse <- function(mydata) {
 #mydata = read.csv(args[1], header=TRUE, sep=",")
 filenames <- list.files("FifteenKnapsacks", pattern="\\.main\\.all\\.csv$", full.names=TRUE)
 
-con <- file("filename.tex", "w")
+con <- file(args[1], "w")
 cat(
-	sprintf("%s \n %s \n %s \n %s \n %s\n %s\n %s \n %s \n",
+	sprintf("%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n",
 		"\\documentclass{article}",
 		"\\begin{document}",
-		"\\begin{table}",
+		"\\begin{table}[h!]",
 		"\\begin{center}",
-		"\\begin{tabular}{| c | c | c | c | c | c | c |}",
+		"\\small",
+		"\\begin{tabular}{| c | c | c | c | c | c | c | c | c | c |}",
 		"\\hline",
-		"instance & completed & avg cpu used & average & max & min & CV \\% \\\\",
+		"inst. & done & AUT & CU & ACT & max & min & CV-T & ObjV & CV-O \\\\",
 		"\\hline")
 	,
 	file=con)
@@ -32,24 +33,27 @@ ldf <- lapply(filenames, function(i){
 		print(length(mydata$Timeout.Reached[mydata$Timeout.Reached == "Yes"]))
 		print(length(mydata$Timeout.Reached['No']))
 		cat(
-			sprintf("\\verb|%s| & %d / %d & %.2f & %.4f & %.4f & %.4f & %.4f \\\\ \n", 
-					basename(i),
+			sprintf("\\verb|%s| & %d / %d & %.4f & %.4f & %.4f & %.4f & %.4f & %.4f & %.2f & %.2f\\\\ \n", 
+					strsplit(basename(i), "\\.")[[1]][1],
 					length(grep('No', mydata$Timeout.Reached)),
 					length(mydata$Timeout.Reached),
-					mean(mydata$CPU.Time)/mean(mydata$User.Time),
 					mean(mydata$User.Time)/60,
-					max(mydata$User.Time)/60,
-					min(mydata$User.Time)/60,
-					(sd(mydata$User.Time)/mean(mydata$User.Time))*100
-					
+					mean(mydata$CPU.Time)/mean(mydata$User.Time),
+					mean(mydata$CPU.Time)/60,
+					max(mydata$CPU.Time)/60,
+					min(mydata$CPU.Time)/60,
+					(sd(mydata$CPU.Time)/mean(mydata$CPU.Time))*100,
+					mean(mydata$Objval),
+					(sd(mydata$Objval)/mean(mydata$Objval))*100
 			),
 			file=con
 		)
 	} else {
 		cat(
-			sprintf("\\verb|%s| & %d & %s & %s & %s & %s & %s & %s  \\\\ \n", 
-					basename(i),
+			sprintf("\\verb|%s| & %d & %s & %s & %s & %s & %s & %s & \\\\ \n", 
+					strsplit(basename(i), "\\.")[[1]][1],
 					0,
+					"NA",
 					"NA",
 					"NA",
 					"NA",
@@ -62,7 +66,7 @@ ldf <- lapply(filenames, function(i){
 	}
 })
 
-cat(sprintf("\\hline\n\\end{tabular}\n\\end{center}\n\\end{table}\n\\end{document}"), file = con);
+cat(sprintf("\\hline\n\\end{tabular}\n\\caption{:}\n\\label{table:}\n\\end{center}\n\\end{table}\n\\end{document}"), file = con);
 
 close(con);
 
--- Benchmark/benchmark.sh
+++ Benchmark/benchmark.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
-iteration_per_instance=2
-output_directory=FifteenKnapsacks
+iteration_per_instance=5
+max_line=$(($iteration_per_instance+1))
+output_directory=FiveKnapsacks/
 program=../Multi3BKP/main
-program_type=main.all
-small_pause=40
-big_pause=80
-regex=20_1.dat
-timeout=7200
-it=0
-number_of_timeouts=0
+program_type=multi.all.E
+small_pause=1
+regex=10_1.dat
+timeout=6300
 
 mkdir $output_directory
 for filename in ../MultiInstance/$output_directory/$regex; do
-	#if [ $((it)) -ne 0 ]; then
-	#	echo "Sleep $big_pause seconds to cool down the system"
-	#	sleep $big_pause
-	#fi;
+	
+	
 	number_of_timeouts=0
 	echo $filename
 	last_part_of_path=`basename -s .dat $filename`
@@ -26,10 +22,21 @@ for filename in ../MultiInstance/$output_directory/$regex; do
 	if [ ! -e "$output" ]; then
 		echo "create $output"
 		echo "User.Time, CPU.Time, Objval, Timeout.Reached" > $output
-	fi;
-		for (( i=1; i<=iteration_per_instance; i++ ))
-		do
-			echo "Starting command: $program $filename -t $timeout -b -x -y -z"
+	fi
+	
+	
+	
+	for (( i=1; i<=iteration_per_instance; i++ ))
+	do
+			
+			lines=`cat $output | wc -l`
+			echo "lines = $lines"
+			if [ "$lines" -ge "$max_line" ]; then #break if the 6 lines are reached
+				break;
+			fi
+			
+			
+			echo "Starting command: $program $filename -t $timeout -o /tmp/output.$last_part_of_path.$i.$program_type.csv -b -x -y -z"
 			out=`$program $filename -t $timeout -b -x -y -z`
 			return_value=$?;
 			if [ $return_value -eq 124 ]; then
@@ -37,11 +44,9 @@ for filename in ../MultiInstance/$output_directory/$regex; do
 				echo "Timeout Reached. $output not successful";
 				out_array=(`echo $out | tr -d ","`)
 				echo "$out, Yes" >> $output
-				#if [ $number_of_timeouts -eq 4 ]; then
-				#	break;
-				#fi;
 			elif [ $return_value -eq 1 ]; then
 				echo "An error occured. Maybe no solution was found";
+				echo "infeasible" >> $output
 				break;
 			else
 				echo "$out, No" >> $output
@@ -50,6 +55,4 @@ for filename in ../MultiInstance/$output_directory/$regex; do
 			echo "Sleep $small_pause seconds to cool down processor"
 			sleep $small_pause;
 		done;
-	
-	it=$((it + 1))
 done;
